6bc8be6cf1252db05e8b8e29c21693c5a19f2c78
Unknown <pablo.garcia@cwi.com.br>
2018-05-27 23:26:56 -0300

    Changing number of Peers

--- Assets/Scripts/Main Menu.gd
+++ Assets/Scripts/Main Menu.gd
@@ -65,7 +65,7 @@ func _set_status(text,isok):
 func _on_Start_Button_pressed():
 	var host = NetworkedMultiplayerENet.new()
 	host.set_compression_mode(NetworkedMultiplayerENet.COMPRESS_RANGE_CODER)
-	var err = host.create_server(DEFAULT_PORT,0) # max: 1 peer, since it's a 2 players game
+	var err = host.create_server(DEFAULT_PORT,1) # max: 1 peer, since it's a 2 players game
 	if (err!=OK):
 		#is another server running?
 		_set_status("Can't host, address in use.",false)
