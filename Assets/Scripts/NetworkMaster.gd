42a3d78f0f24c5fce4cdb7fdfd2e4c0c4a48b9af
Unknown <pablo.garcia@cwi.com.br>
2018-05-29 11:12:04 -0300

    Adding player to process/process to player transfer functions

--- Assets/Scripts/NetworkMaster.gd
+++ Assets/Scripts/NetworkMaster.gd
@@ -1,6 +1,7 @@
 extends Node
 
 var playerObject = load("res://Assets/Models/Objects/Player.tscn")
+var boxHolder = ("BoxHolder")
 
 func _ready():
 	#instert player 1 onto scene
@@ -28,39 +29,38 @@ func _set_players_on_network(botP1, botP2):
 		#get_parent().get_node("Player_01").set_network_master(get_tree().get_network_connected_peers()[0])
 	pass
 
-func _on_player_deliver_box_to_process(player, box, process):
-	pass
+func _on_player_deliver_box_to_process(player, process, box):
+	print("Network master delivers box: ", box, " from ", player, " to ", process);
+	
+	_deliver_box_to_process_from_player(player, process, box);
+	rpc("_deliver_box_to_process_from_player",player, process, box);
 
 func _on_player_ask_box_from_process(player, process):
-	#print("NetPlayer: ", player, " asked for a box from: ", process)
-	var box = rpc("get_box_from_process", process);
-	print("NetPlayer: ", player, " asked for a box: ", box, " from: ", process)
-	rpc("_deliver_box_to_player_from_process", player, box, process)
-	pass
+	var box = get_parent().get_node(process).can_deliver_box()
+	
+	_deliver_box_to_player_from_process(player, box, process)
+	rpc("_deliver_box_to_player_from_process", player, box, process)	
 
-sync func get_box_from_process(process):
-	var box = process.can_deliver_box();
-	if (box != null):
-		print("Process:", process, " can deliver box: ", box)
-		return box;
+remote func _deliver_box_to_player_from_process(player, box, process):
+	if(box != null):
+		var boxToDeliver = get_parent().get_node(process).get_node("BoxHolder").get_node(box)
+		get_parent().get_node(process).get_node("BoxHolder").call_deferred("remove_child",boxToDeliver);
+		get_parent().get_node(process).hasBox = false;
+		get_parent().get_node(process).processStatus = "Processing"
+		get_parent().get_node(player).get_node("BoxHolder").call_deferred("add_child",boxToDeliver);
+		get_parent().get_node(player).hasBox = true;
 	else:
-		print("Process: ", process, " cannot deliver box.")
-		return null;
+		print("No box found.")
 	pass
 
-sync func _deliver_box_to_player_from_process(player, box, process):
-	print("Network master delivers box: ", box, " from process", process)
-	process.remove_child(box);
-	player.get_node("PlayerTop").add_child(box);
-	pass
-
-sync func _deliver_box_to_process_from_player(player, box, process):
-	pass
-
-
-
-func _on_Player_01_ask_box_from_process(player, process):
-	print("NetPlayer: ", player, " asked for a box from: ", process)
-	#var box = get_box_from_process(process);
-	#rpc("_deliver_box_to_player_from_process", player, box, process)
+remote func _deliver_box_to_process_from_player(player, process, box):
+	if(box != null):
+		var boxToDeliver = get_parent().get_node(player).get_node("BoxHolder").get_node(box);
+		get_parent().get_node(player).get_node("BoxHolder").call_deferred("remove_child", boxToDeliver);
+		get_parent().get_node(player).hasBox = false;
+		get_parent().get_node(process).get_node("BoxHolder").call_deferred("add_child", boxToDeliver);
+		get_parent().get_node(process).hasBox = true;
+		pass
+	else:
+		print("No box found.")	
 	pass
