ab72c8a0bf014d730e72e4d344ff74011979cc10
Unknown <pablo.garcia@cwi.com.br>
2018-05-29 11:10:43 -0300

    Removing old code from box

--- Assets/Scripts/Box.gd
+++ Assets/Scripts/Box.gd
@@ -16,12 +16,7 @@ func _ready():
 	
 	pass
 
-func _process(delta):
-	#rpc_unreliable("current_position", transform);
-	pass
-
-sync func current_position(box_current_position):
-	transform = box_current_position 
+func _process(delta):	
 	pass
 
 func get_Color():
